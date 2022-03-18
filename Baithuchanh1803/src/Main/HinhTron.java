038221e2e471e8387e128de18c74b7cc35198714
Trang007 <dangthuytrang007@gmail.com>
2022-03-18 19:16:44 +0700

    commit note

--- Baithuchanh1803/src/Main/HinhTron.java
+++ Baithuchanh1803/src/Main/HinhTron.java
@@ -1,22 +1,25 @@
+package Main;
 import java.util.Scanner;
 public class HinhTron {
     final float PI = 3.14f;
-    float r, cv, dt;
-    void nhapbankinh() {
+    static float r;
+    float cv;
+    float dt;
+    public static void nhapbankinh() {
         System.out.print("Nhap ban kinh: ");
         Scanner sc = new Scanner(System.in);
         r = sc.nextFloat();
     }
-    void tinhchuvi() {
+    public void tinhchuvi() {
         cv = 2 * PI * r;
     }
-    void tinhdientich() {
+    public void tinhdientich() {
         dt = PI * r * r;
     }
-    void inchuvi() {
+    public void inchuvi() {
         System.out.println("Chu vi hinh tron = " + cv);
     }
-    void indientich() {
+    public void indientich() {
         System.out.println("Dien tich hinh tron = " + dt);
     }
     public static void main(String[] args) {
